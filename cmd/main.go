2b4f344527480ceb056e773d9414049954a670c3
Nijat Mahmudov <n.e.mahmudov@gmail.com>
2020-03-25 16:43:09 +0400

    Add bitfields based logger

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/nij4t/alorithms-and-data-structures/pkg/bitfields"
 	"github.com/nij4t/alorithms-and-data-structures/pkg/hashing"
 )
 
@@ -10,4 +11,10 @@ func main() {
 	for _, item := range data {
 		println(hashing.CalculateHash(item, 10))
 	}
+
+	bitfields.Log("info", bitfields.INFO)
+	bitfields.Log("notice", bitfields.NOTICE)
+	bitfields.Log("warn", bitfields.WARN)
+	bitfields.Log("error", bitfields.ERROR)
+	bitfields.Log("debug", bitfields.DEBUG)
 }
---
+++ pkg/bitfields/bitfields.go
@@ -0,0 +1,37 @@
+package bitfields
+
+import "fmt"
+
+const (
+	INFO   = 0x01 // 00000001
+	NOTICE = 0x02 // 00000010
+	WARN   = 0x03 // 00000011
+	ERROR  = 0x04 // 00000100
+	DEBUG  = 0x05 // 00000101
+	MASK   = 0x07 // 00000111
+)
+
+func Log(msg string, options uint8) {
+
+	const (
+		InfoColor    = "\033[1;34m%s\033[0m"
+		NoticeColor  = "\033[1;36m%s\033[0m"
+		WarningColor = "\033[1;33m%s\033[0m"
+		ErrorColor   = "\033[1;31m%s\033[0m"
+		DebugColor   = "\033[0;36m%s\033[0m"
+	)
+
+	switch options & 0x07 {
+	case INFO:
+		fmt.Printf(InfoColor, msg)
+	case NOTICE:
+		fmt.Printf(NoticeColor, msg)
+	case WARN:
+		fmt.Printf(WarningColor, msg)
+	case ERROR:
+		fmt.Printf(ErrorColor, msg)
+	case DEBUG:
+		fmt.Printf(DebugColor, msg)
+	}
+	fmt.Println("")
+}
